3d0e5a56740817d296e095b3be5140f7306b50ad
荻原由佳 <ogiharayuka@ogiwarayukanoMacBook.local>
2019-10-22 05:14:51 +0900

    Execute 'rails db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_21_194649) do
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
